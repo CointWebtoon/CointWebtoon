c9d3703d2a0ed9222c2ccbc151a183b70760332c
jungmintaeng <jungmin7623@naver.com>
2017-02-27 15:51:40 +0900

    EpisodeActivity

--- app/src/main/java/com/jmapplication/com/episodeactivity/EpisodeActivity.java
+++ app/src/main/java/com/jmapplication/com/episodeactivity/EpisodeActivity.java
@@ -47,27 +47,6 @@ public class EpisodeActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
 
         listView = (ListView)findViewById(R.id.episodeListView);
-        listView.setAdapter(new BaseAdapter() {
-            @Override
-            public int getCount() {
-                return 0;
-            }
-
-            @Override
-            public Object getItem(int position) {
-                return null;
-            }
-
-            @Override
-            public long getItemId(int position) {
-                return 0;
-            }
-
-            @Override
-            public View getView(int position, View convertView, ViewGroup parent) {
-                return null;
-            }
-        });
     }
 
     @Override
---
+++ app/src/main/java/com/jmapplication/com/episodeactivity/EpisodeAdapter.java
@@ -0,0 +1,31 @@
+package com.jmapplication.com.episodeactivity;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by jm on 2017-02-27.
+ */
+
+public class EpisodeAdapter extends BaseAdapter{
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
