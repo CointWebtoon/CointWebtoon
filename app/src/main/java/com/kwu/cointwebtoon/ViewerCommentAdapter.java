29f99923c4c6b368b3d7bf4fa6d55c3c0c8d2fb5
shees <shees93@naver.com>
2017-05-18 13:27:58 +0900

    리소스 수정 & 댓글 껍데기

---
+++ app/src/main/java/com/kwu/cointwebtoon/ViewerCommentAdapter.java
@@ -0,0 +1,30 @@
+package com.kwu.cointwebtoon;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by shees on 2017-05-18.
+ */
+
+public class ViewerCommentAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+    @Override
+    public Object getItem(int position) {
+        return 0;
+    }
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+
+        return view;
+    }
+}
