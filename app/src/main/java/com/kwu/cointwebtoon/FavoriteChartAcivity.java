438b4bd271353e3e095a03908813df8446172953
eunjooChoi <epcej0020@gmail.com>
2017-05-19 06:21:05 +0900

    취향분석 구현( 버벅거림 있음)

--- app/src/main/java/com/kwu/cointwebtoon/FavoriteChartAcivity.java
+++ app/src/main/java/com/kwu/cointwebtoon/FavoriteChartAcivity.java
@@ -1,11 +1,16 @@
 package com.kwu.cointwebtoon;
 
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
@@ -13,30 +18,37 @@ import com.github.mikephil.charting.formatter.PercentFormatter;
 import com.github.mikephil.charting.highlight.Highlight;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.github.mikephil.charting.utils.ColorTemplate;
+import com.kwu.cointwebtoon.DataStructure.Episode;
+import com.kwu.cointwebtoon.DataStructure.Genre;
 
 import java.util.ArrayList;
 
 public class FavoriteChartAcivity extends TypeKitActivity {
     private RelativeLayout favoriteLayout;
+    private Context mContext;
     private PieChart pieChart;
-    private  float[] yData = {5,10,15,20,30,40};
-    private String[] xData = {"A","B","C","D","E","F"};
+    private float[] yData = {0,0,0,0,0,0,0,0,0,0};
+    private int[] count = {0,0,0,0,0,0,0,0,0,0};
+    private String[] xData = {"코믹", "일상", "순정", "스릴러", "판타지", "역사", "드라마", "스포츠", "액션", "기타"};
+    private COINT_SQLiteManager coint_sqLiteManager;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.favorite_chart_activity);
+        mContext = this;
 
-        favoriteLayout = (RelativeLayout)findViewById(R.id.favoriteChart);
-        pieChart = (PieChart)findViewById(R.id.pieChart);
+        favoriteLayout = (RelativeLayout) findViewById(R.id.favoriteChart);
+        pieChart = (PieChart) findViewById(R.id.pieChart);
         //add pie chart to layout
 
         favoriteLayout.setBackgroundColor(Color.WHITE);
 
         //configure pie chart
         pieChart.setUsePercentValues(true);
-        pieChart.setDescription("Chart Test");
 
         //enable hole and configure
+        pieChart.setDescription("");
+        pieChart.setEnabled(false);
         pieChart.setDrawHoleEnabled(true);
         pieChart.setHoleColor(1); //왜 없어 여긴
         pieChart.setHoleRadius(7);
@@ -51,76 +63,131 @@ public class FavoriteChartAcivity extends TypeKitActivity {
             @Override
             public void onValueSelected(Entry entry, int i, Highlight highlight) {
                 // display msg when value selected
-                if(entry==null)
+                if (entry == null)
                     return;
 
-                Toast.makeText(FavoriteChartAcivity.this, xData[entry.getXIndex()]+"="+entry.getVal()+"%",Toast.LENGTH_SHORT).show();
+                Toast.makeText(FavoriteChartAcivity.this, xData[entry.getXIndex()] + "=" + entry.getVal() + "%", Toast.LENGTH_SHORT).show();
+                //startActivity(new Intent(mContext, GenreActivity.class));
+                //누르면 해당 장르로 이동!!!하도록 구현
             }
 
             @Override
             public void onNothingSelected() {
-
             }
         });
 
         addData();
 
         //customize legends
-/*        Legend legend = pieChart.getLegend();
-        legend.setPosition(Legend.LegendPosition.RIGHT_OF_CHART);
-        legend.setXEntrySpace(7);
-        legend.setYEntrySpace(5);*/
+        Legend legend = pieChart.getLegend();
+        legend.setEnabled(false);
     }
 
-    private void addData(){
-        ArrayList<Entry> yVals1 = new ArrayList<Entry>();
+    private void addData() {
+        ArrayList<Entry> yVals1 = new ArrayList<>();
+        int genreIndex=0;
+        coint_sqLiteManager = COINT_SQLiteManager.getInstance(mContext);
+        ArrayList<Genre> genreArrayList;
+        genreArrayList = coint_sqLiteManager.getGenreAll();
+        for(int i=0;i<genreArrayList.size();i++){
+            Log.i("GENRE : " ,genreArrayList.get(i).getGenre() );
+            if(genreArrayList.get(i).getGenre().equals("COMIC")){
+                genreIndex = 0;
+            }else if(genreArrayList.get(i).getGenre().equals("DAILY")){
+                genreIndex = 1;
+            }else if(genreArrayList.get(i).getGenre().equals("PURE")){
+                genreIndex = 2;
+            }else if(genreArrayList.get(i).getGenre().equals("THRILL")){
+                genreIndex = 3;
+            }else if(genreArrayList.get(i).getGenre().equals("FANTASY")){
+                genreIndex = 4;
+            }else if(genreArrayList.get(i).getGenre().equals("HISTORICAL")){
+                genreIndex = 5;
+            }else if(genreArrayList.get(i).getGenre().equals("DRAMA")){
+                genreIndex = 6;
+            }else if(genreArrayList.get(i).getGenre().equals("SPORTS")){
+                genreIndex = 7;
+            }else if(genreArrayList.get(i).getGenre().equals("ACTION")){
+                genreIndex = 8;
+            }else{
+                genreIndex = 9;
+            }
 
-        for(int i=0;i<yData.length;i++)
-            yVals1.add(new Entry(yData[i],i));
+            Cursor cursor = coint_sqLiteManager.getEpisodes(genreArrayList.get(i).getId());
+            while(cursor.moveToNext()){
+                if(cursor.getInt(8)==1){
+                    count[genreIndex]++;
+                }
+            }
+            cursor.close();
+        }
 
-        ArrayList<String> xVals = new ArrayList<String>();
+        int totalread = 0;
+        for(int i=0;i<count.length;i++){
+            totalread+=count[i];
+        }
 
-        for(int i=0;i<xData.length;i++)
-            xVals.add(xData[i]);
+        if(totalread==0){
+            startActivity(new Intent(mContext, GenreActivity.class));
+            finish();
+        }else {
 
-        //create pie data set
-        PieDataSet dataSet  = new PieDataSet(yVals1, "TestTesT");
-        dataSet.setSliceSpace(3);
-        dataSet.setSelectionShift(5);
+            for(int i=0;i<count.length;i++){
+                yData[i] = (float)count[i]/totalread*100;
+                Log.i("들어왔다", Integer.toString(totalread));
+            }
 
-        //add many colors
-        ArrayList<Integer> colors = new ArrayList<Integer>();
+            for (int i = 0; i < yData.length; i++){
+                if(yData[i]>0){
+                    yVals1.add(new Entry(yData[i], i));
+                }
+            }
 
-        for (int c: ColorTemplate.VORDIPLOM_COLORS)
-            colors.add(c);
+            ArrayList<String> xVals = new ArrayList<>();
 
-        for (int c: ColorTemplate.JOYFUL_COLORS)
-            colors.add(c);
+            for (int i = 0; i < xData.length; i++)
+                xVals.add(xData[i]);
 
-        for (int c: ColorTemplate.COLORFUL_COLORS)
-            colors.add(c);
+            //create pie data set
+            PieDataSet dataSet = new PieDataSet(yVals1, "Favorite Chart");
+            dataSet.setSliceSpace(1);
+            dataSet.setSelectionShift(5);
 
-        for (int c: ColorTemplate.LIBERTY_COLORS)
-            colors.add(c);
+            //add many colors
+            ArrayList<Integer> colors = new ArrayList<>();
 
-        for (int c: ColorTemplate.PASTEL_COLORS)
-            colors.add(c);
+            for (int c : ColorTemplate.VORDIPLOM_COLORS)
+                colors.add(c);
 
-        colors.add(ColorTemplate.getHoloBlue());
-        dataSet.setColors(colors);
+            for (int c : ColorTemplate.JOYFUL_COLORS)
+                colors.add(c);
 
-        //instantiate pie data object now
-        PieData data = new PieData(xVals,dataSet);
-        data.setValueFormatter(new PercentFormatter());
-        data.setValueTextSize(11f);
-        data.setValueTextColor(Color.BLACK);
+            for (int c : ColorTemplate.COLORFUL_COLORS)
+                colors.add(c);
 
-        pieChart.setData(data);
+            for (int c : ColorTemplate.LIBERTY_COLORS)
+                colors.add(c);
 
-        //undo all highlights
-        pieChart.highlightValues(null);
+            for (int c : ColorTemplate.PASTEL_COLORS)
+                colors.add(c);
 
-        //update pie chart
-        pieChart.invalidate();
+            colors.add(ColorTemplate.getHoloBlue());
+            dataSet.setColors(colors);
+
+            //instantiate pie data object now
+            PieData data = new PieData(xVals, dataSet);
+            data.setValueFormatter(new PercentFormatter());
+            data.setValueTextSize(11f);
+            data.setValueTextColor(Color.BLACK);
+
+            pieChart.setData(data);
+
+            //undo all highlights
+            pieChart.highlightValues(null);
+
+            //update pie chart
+            pieChart.invalidate();
+        }
     }
+
 }
