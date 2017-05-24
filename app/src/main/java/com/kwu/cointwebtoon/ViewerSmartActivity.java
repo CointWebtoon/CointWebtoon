63e1cf58f7d556c60d2e204fe5cdc9863ebd97b7
jungmintaeng <jungmin7623@naver.com>
2017-05-25 01:31:17 +0900

    스마트툰 액티비티 데이터바인딩 이상한거 떠서 다시 푸시푸시

--- app/src/main/java/com/kwu/cointwebtoon/ViewerSmartActivity.java
+++ app/src/main/java/com/kwu/cointwebtoon/ViewerSmartActivity.java
@@ -2,7 +2,6 @@ package com.kwu.cointwebtoon;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.databinding.tool.util.L;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.AsyncTask;
@@ -14,7 +13,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.RatingBar;
 import android.widget.RelativeLayout;
